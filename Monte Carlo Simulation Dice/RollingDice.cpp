ac1fcbf68bd5a93d34e50a784f14291d186b6585
jamesleetu <leetaeung@gmail.com>
2021-09-09 22:53:27 +0900

    Monte Carlo Simulation

---
+++ Monte Carlo Simulation Dice/RollingDice.cpp
@@ -0,0 +1,61 @@
+﻿#include "pch.h"
+#include "tipsware.h"
+#include "time.h"
+#define _USE_MATH_DEFINES
+#include "math.h"
+
+NOT_USE_MESSAGE
+
+#define MAX_TRY 10000
+
+int main()
+{
+	//srand(time(NULL));
+
+	SelectPenObject(RGB(0, 0, 0));
+
+	int hit = 0;
+	int miss = 0;
+	float relProb = 5.0 / 36.0;
+	float prob = 0;
+
+	int randTemp1;
+	int randTemp2;
+
+	int arr1[MAX_TRY];
+	int arr2[MAX_TRY];
+	float count = 0;
+	// TODO 
+
+	for (int i = 0; i < MAX_TRY; i++) {
+		randTemp1 = rand() % 6 + 1;
+		randTemp2 = rand() % 6 + 1;
+
+		arr1[i] = randTemp1;
+		arr2[i] = randTemp2;
+	}
+
+	for (int i = 0; i < MAX_TRY; i++) {
+		if (arr1[i] + arr2[i] == 8) {
+			count += 1.0;
+			TextOut(120 * (i / 20), 20 * (i % 20), "try %d: %d,%d (hit!)", 1 + i, arr1[i], arr2[i]);
+		}
+		else {
+			TextOut(120 * (i / 20), 20 * (i % 20), "try %d: %d,%d", 1 + i, arr1[i], arr2[i]);
+		}
+	}
+	//TextOut(120 * (0 / 20), 20 * (0 % 20), "try %d: %d,%d (hit!)", 0 + 1, 6, 2);
+	//TextOut(120 * (1 / 20), 20 * (1 % 20), "try %d: %d,%d", 0 + 2, 6, 6);
+	
+	prob = count / MAX_TRY;
+
+	SetTextColor(RGB(128, 0, 0));
+	// 실제 주사위 값
+	TextOut(0, 420, "실제 값: %.6f", relProb);
+	// 계산된 값
+	TextOut(0, 440, "계산된 값: %.6f", prob);
+
+	ShowDisplay();
+
+	return 0;
+}
---
+++ Monte Carlo Simulation Histogram/DistributHistogram.cpp
@@ -0,0 +1,70 @@
+﻿#include "pch.h"
+#include "tipsware.h"
+
+NOT_USE_MESSAGE
+
+#define GRAPH_WIDTH   520  // 그래프의 폭
+#define GRAPH_HEIGHT  590  // 그래프의 높이
+
+#define CENTER_X      (GRAPH_WIDTH/2)  // 그래프의 중심 점 (x)
+#define CENTER_Y      (GRAPH_HEIGHT/2) // 그래프의 중심 점 (y)
+
+#define ZOOM_LEVEL    10
+
+#define MAX_TRY 3000000
+#define SCALING_FACTOR 100
+#define RANGE 100
+
+void DrawBackgroundOfGraph()
+{
+    Rectangle(20, 0, GRAPH_WIDTH, GRAPH_HEIGHT, RGB(0, 0, 0));
+
+    SelectFontObject("굴림", 10, 1);
+    SetTextColor(RGB(0, 0, 0));
+
+    for (int i = 0; i < 10; i++) {
+        TextOut(0, i * (GRAPH_HEIGHT / 10), "%d", (600-i*60) * SCALING_FACTOR);
+        Line(20, i * (GRAPH_HEIGHT/ 10), 25, i * (GRAPH_HEIGHT / 10), RGB(182, 198, 198));
+    }
+
+    for (int i = 0; i < RANGE; i++) {
+        Line(23 + i * 5, GRAPH_HEIGHT - 5, 23 + i * 5, GRAPH_HEIGHT, RGB(182, 198, 198));
+        if (i % 10 == 0) {
+            TextOut(18 + i * 5, GRAPH_HEIGHT + 1, "%d", i);
+        }
+    }
+}
+
+int main()
+{
+    DrawBackgroundOfGraph();
+    HWND g_hWnd = GetForegroundWindow();
+    SetWindowPos(g_hWnd, NULL, 50, 50, 1000, 700, 0);
+
+    int numArr[RANGE] = { 0, };
+    int uniArr[RANGE] = { 0, };
+
+    SelectPenObject(RGB(255, 255, 0));
+
+    int randTemp;
+
+    for (int i = 0; i < MAX_TRY; i++) {
+        randTemp = rand() % RANGE;
+        numArr[randTemp] += 1;
+    }
+    // TODO
+    for (int i = 0; i < RANGE; i++) {
+
+        Rectangle((23 + i * 5) - 1.0, GRAPH_HEIGHT, (23 + i * 5) + 1.0, GRAPH_HEIGHT - (numArr[i] / (GRAPH_HEIGHT / 6)), RGB(0, 0, 255));
+        //for (int j = 0; j < RANGE; j++) {
+        //    Rectangle((23 + i) - 1.0, GRAPH_HEIGHT, (23 + i) + 1.0, GRAPH_HEIGHT - numArr[j], RGB(0, 0, 255));
+        //}
+    }
+    //Rectangle(46 - 1.0, GRAPH_HEIGHT, 46 + 1.0, GRAPH_HEIGHT - 100, RGB(0, 0, 255));
+    //Rectangle(123 - 1.0, GRAPH_HEIGHT, 123 + 1.0, GRAPH_HEIGHT - 250, RGB(0, 0, 255));
+    
+    //
+
+    ShowDisplay();
+    return 0;
+}
